8ae7bc32f29c6ef2fdd5e56740ca5b64eecc2396
hellyp-concettolabs <helly.patel@concettolabs.com>
2024-03-13 17:41:50 +0530

    made subscribe section responsive

--- Ecommerce/src/Components/Body/SubscribeSection/Subscribe.jsx
+++ Ecommerce/src/Components/Body/SubscribeSection/Subscribe.jsx
@@ -1,28 +1,28 @@
 import { Button, Col, Container, Form, Image, Row } from "react-bootstrap"
 import subscribearrow from "../../../assets/subscribearrow.png"
-
+import "./subscribe.scss"
 
 function Subscribe() {
   return (
     <>
         <Container fluid className=" my-5">
             <Container className=" bg-primary rounded-4 ">
-                <Row className=" p-3 align-items-center d-lg-flex d-block gap-lg-0 g-3 ">
-                    <Col className=" d-flex">
+                <Row className=" subscontainer">
+                    <Col className="col-6 subscon1">
                         <Row>
-                            <Col className=" col-lg-10 col-12 text-lg-start text-center  ">
-                                <div className=" text-light fw-bold fs-4 py-2 ">Subscribe to Our Newsletters</div>
-                                <div className=" text-light ">Receive exclusive offers, unique gift ideas, and personalised tips for shopping and selling on BargainFox.</div>
-                            </Col>
-                            <Col className=" d-none d-lg-flex p-0">
-                                 <Image src={subscribearrow} className=" img-fluid "/>
-                            </Col>
+                            <Col className="subscribeheading">Subscribe to Our Newsletters</Col>
                         </Row>
+                         <Row>
+                         <div className="subdesp p-0">Receive exclusive offers, unique gift ideas, and personalised tips for shopping and selling on BargainFox.</div>
+                        </Row>              
                     </Col>
-                    <Col>
-                        <Form className='search d-flex w-100 '>
-                            <Form.Control type="text" placeholder="Enter your email" className='rounded-start-5 rounded-end-0' />
-                            <Button variant="outline-success" className='subscribe rounded-end-5 rounded-start-0 bg-dark text-light'>
+                    <Col className="subarrow">
+                        <Image src={subscribearrow} className=" img-fluid"/>
+                    </Col>
+                    <Col className=" col-5 subscon2">
+                        <Form className='d-flex w-100 '>
+                            <Form.Control type="text" placeholder="Enter your email" className='subsearch rounded-start-5 rounded-end-0' />
+                            <Button  className='subnow rounded-end-5 rounded-start-0 col-sm-4 col-6 '>
                                 Subscibe Now
                             </Button>
                         </Form>
